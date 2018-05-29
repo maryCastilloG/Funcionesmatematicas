20463c3a2e71dd81215c280c1e171d493075a00f
maryCastilloG <maryecg@gmail.com>
2018-05-29 13:23:45 -0400

    funciones

---
+++ app.js
@@ -0,0 +1,13 @@
+    function add(number1,number2){
+return number1 + number2;;
+    }
+
+    let multiply = function(number1,number2){
+let product = number1 * number2;
+return product;
+
+    }
+let cielo = add(9824,777);
+    
+    console.log(multiply(36325,cielo));
+    console.log(multiply(36325,add(9824,777)));
